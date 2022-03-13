43009b5583b9f6ee1f1ecaf5715c19aa480979e1
FlavioLAMartins <flavio_martins11@hotmail.com>
2022-03-13 13:58:54 -0300

    exercicio modulos

--- exercicio-modulos/src/app/features/collaborators/components/collaborators-list/collaborators-list.component.ts
+++ exercicio-modulos/src/app/features/collaborators/components/collaborators-list/collaborators-list.component.ts
@@ -6,7 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./collaborators-list.component.scss']
 })
 export class CollaboratorsListComponent implements OnInit {
-
+  title: string = 'Colaboradores'
+  collaborators = [
+    { id: 1, nome: 'Nathan Carlos', salario: 7000, cargo: 'Dev Pl' },
+    { id: 2, nome: 'David Carlos', salario: 3000, cargo: 'Dev Jr' },
+    { id: 3, nome: 'Mateus Augusto', salario: 3500, cargo: 'Dev Jr' },
+    { id: 4, nome: 'Alberto Silva', salario: 10000, cargo: 'Dev Sr' },
+    { id: 5, nome: 'Rogerio Souza', salario: 9000, cargo: 'Dev Sr' },
+    { id: 6, nome: 'Gabriel Miguel', salario: 9500, cargo: 'Dev Sr' },
+    { id: 7, nome: 'Alan Jhonnes', salario: 11000, cargo: 'Arquiteto' }
+  ];
+  collaborator = { id: 7, nome: 'Nathan Carlos', salario: 7000, cargo: 'Dev Pl' }
   constructor() { }
 
   ngOnInit(): void {
